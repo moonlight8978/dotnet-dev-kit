9f38c42d10d5450a1fb89e0734f2415aaa4e13ed
_MoonLight_ <moonlight8978@gmail.com>
2023-10-06 08:51:17 +0700

    Update

---
+++ DummyApp/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using DotnetDevelopmentSdk.Lib.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DummyApp.Database;
+
+public class AppDbContext : BasePostgreSqlDbContext<AppDbContext, DatabaseConfiguration>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions,
+        IOptions<DatabaseConfiguration> databaseConfiguration) : base(dbContextOptions, databaseConfiguration)
+    {
+    }
+}
--- DummyApp/Models/WeatherForecastModel.cs
+++ DummyApp/Models/WeatherForecastModel.cs
@@ -1,14 +1,28 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace DummyApp.Models;
 
+[Table("weather_forecasts")]
+[Index(nameof(Date), IsUnique = true)]
 public class WeatherForecastModel
 {
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("date")]
     public DateTime Date { get; set; }
 
+    [Column("temperature_c")]
     public int TemperatureC { get; set; }
 
+    [NotMapped]
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    [Column("summary")]
     public string? Summary { get; set; }
 }
